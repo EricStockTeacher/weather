0cdeec52eee44901a4c89d340588844b516ba7f6
Eric Stock <eric.stock@K202Domain.CSN.com>
2024-05-16 10:59:04 -0230

    Added basic rendering of weather data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import localweather from './StJohnsWeather.json';
 
 function App() {
   return (
-    <h2>Eric's Weather</h2>
+    <>
+      <h2>Eric's Weather</h2>
+      <WeatherList weather={localweather}></WeatherList>
+    </>
+  );
+}
+
+function WeatherList({weather}) {
+  return (
+    <>
+      <h2>{weather.city}</h2>
+      {weather.forecast.map( day => <Weather info={day}></Weather>)}
+    </>
+  );
+}
+
+function Weather({info}) {
+  return (
+  <>
+    <p>{info.day}</p>
+    <p>{info.temp}</p>
+    <p>{info.precip}</p>
+    <p>{info.wind}</p>
+    <br></br>
+  </>
   );
 }
 
